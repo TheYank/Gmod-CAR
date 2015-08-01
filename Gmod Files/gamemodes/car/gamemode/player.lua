5bbd8bfdea1e7950301538304ff500bf8020456f
TheYank <ostrich.inventor@gmail.com>
2015-08-01 08:13:42 -0700

    Car.txt Updates
    Icon still not showing up

--- Gmod Files/gamemodes/car/gamemode/player.lua
+++ Gmod Files/gamemodes/car/gamemode/player.lua
@@ -3,7 +3,7 @@ local ply = FindMetaTable("Player")
 local teams = {}
 
 teams[1] = {name = "Cops", color = Vector( .2, .2, 1.0 ), weapons = {"weapon_smg1", "weapon_pistol", "weapon_crowbar", "weapon_frag", "weapon_frag"} }
-teams[2] = {name = "Rapists", color = Vector( 1.0, .2, .2 ), weapons = {"weapon_smg1", "weapon_pistol", "weapon_crowbar", "weapon_frag", "weapon_frag"} }
+teams[2] = {name = "Robbers", color = Vector( 1.0, .2, .2 ), weapons = {"weapon_smg1", "weapon_pistol", "weapon_crowbar", "weapon_frag", "weapon_frag"} }
 
 function ply:SetGamemodeTeam( n )
 	if not teams[n] then return end
--- Gmod Files/gamemodes/car/gamemode/shared.lua
+++ Gmod Files/gamemodes/car/gamemode/shared.lua
@@ -1,10 +1,10 @@
-GM.Name = "Cops & Rapists"
-GM.Author = "SixthSauce"
+GM.Name = "Cops & Robbers"
+GM.Author = "DerpSquad"
 GM.Email = "N/A"
-GM.Website = "N/A"
+GM.Website = "https://github.com/TheYank/Gmod-CAR"
 
 team.SetUp( 1, "Cops", Color(0, 0, 255) )
-team.SetUp( 2, "Rapists", Color(255, 0, 0) )
+team.SetUp( 2, "Robbers", Color(255, 0, 0) )
 
 function GM:Initialize()
 	self.BaseClass.Initialize( self )
